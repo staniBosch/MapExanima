7e5b5660d5f4d556510b68b16c19d630eaa43be1
staniBosch <stani300191@yahoo.de>
2022-08-04 17:10:26 +0200

    small fixes, added enhancements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,212 +1,300 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Memory.Win32;
+using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-using System.Diagnostics;
-using Memory.Win32;
-using Memory.Utils;
-using System.Runtime.InteropServices;
-using Memory.Win64;
 
 namespace MapExanima
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    ///    
     /// </summary>
+    /// 
+    ///  The important stuff happens in ReadMemoryOfExanima()
+    /// <see cref="ReadMemoryOfExanima"/>
     public partial class MainWindow : Window
     {
-        WindowSinker sinker;
+        //The small map window size
+        private const short SMWindow = 300;
+        //if the window is big rezised
         bool isBig = false;
+        //check for doubleclick on map
         static bool isDoubleClick = false;
+        float[] posXY;
+        //MAIN Thread for getting information and refreshing map
+        Thread workThread;
+        bool runThread = true;
         public MainWindow()
         {
-            //sinker = new WindowSinker(this);
-            //sinker.Sink();
-            InitializeComponent(); 
-
-            this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map1Exanima.png"));
-            new Thread(new ThreadStart(ReadMemoryOfExanima)).Start(); 
+            posXY = new float[2];
+            InitializeComponent();
+            workThread = new Thread(new ThreadStart(ReadMemoryOfExanima));
+            workThread.Start();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(DropDownMap.SelectedIndex);
+            //System.Diagnostics.Debug.WriteLine(DropDownMap.SelectedIndex);
             switch (DropDownMap.SelectedIndex)
             {
-                case 0: 
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map1Exanima.png"));
+                case 0:
+                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/Maps/[ID2]Map_LVL1.png"));
                     break;
-                case 1: 
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map2Exanima.png"));
+                case 1:
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID3]Map_LVL2.png"));
                     break;
                 case 2:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map2-1Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID4]Map_LVL3.png"));
                     break;
                 case 3:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map3Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID5]Map_Catacombs.png"));
                     break;
                 case 4:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map4Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID6]Map_Archive.png"));
                     break;
                 case 5:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map5Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID7]Map_Crossroads.png"));
                     break;
                 case 6:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map5-1Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID8]Map_Golems.png"));
                     break;
                 case 7:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map6Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID9]Map_CrossroadsSewers.png"));
                     break;
                 case 8:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map7Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID10]Map_Market.png"));
                     break;
                 case 9:
-                    this.MapImageElement.Source = new BitmapImage(new Uri("pack://application:,,,/MapExanima;component/Map7-1Exanima.png"));
+                    this.MapImageElement.Source = new BitmapImage(new Uri("application:///[ID11]Map_MarketSewer.png"));
                     break;
             }
         }
         private void close_btn_Click(object sender, RoutedEventArgs e)
         {
+            runThread = false;
             this.Close();
-        }       
+        }
         private void window_MouseMove(object sender, MouseEventArgs e)
-        {           
+        {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                this.DragMove();                
+                this.DragMove();
             }
-        }       
+        }
         private void KeyDown_Event(object s, KeyEventArgs e)
         {
             Thickness thicknessMap = this.MapImageElement.Margin;
-            
-            if (e.Key == Key.Up) { 
-                this.MapImageElement.Margin = new Thickness(thicknessMap.Left, thicknessMap.Top + 10, thicknessMap.Right, thicknessMap.Bottom);                
+
+
+            if (e.Key == Key.Up)
+            {
+                this.MapImageElement.Margin = new Thickness(thicknessMap.Left, thicknessMap.Top + 10, thicknessMap.Right, thicknessMap.Bottom);
             }
             if (e.Key == Key.Down)
             {
-                this.MapImageElement.Margin = new Thickness(thicknessMap.Left, thicknessMap.Top - 10, thicknessMap.Right, thicknessMap.Bottom);              
+                this.MapImageElement.Margin = new Thickness(thicknessMap.Left, thicknessMap.Top - 10, thicknessMap.Right, thicknessMap.Bottom);
             }
             if (e.Key == Key.Left)
             {
-                this.MapImageElement.Margin = new Thickness(thicknessMap.Left+10, thicknessMap.Top, thicknessMap.Right, thicknessMap.Bottom);              
+                this.MapImageElement.Margin = new Thickness(thicknessMap.Left + 10, thicknessMap.Top, thicknessMap.Right, thicknessMap.Bottom);
             }
             if (e.Key == Key.Right)
             {
-                this.MapImageElement.Margin = new Thickness(thicknessMap.Left-10, thicknessMap.Top, thicknessMap.Right, thicknessMap.Bottom);             
+                this.MapImageElement.Margin = new Thickness(thicknessMap.Left - 10, thicknessMap.Top, thicknessMap.Right, thicknessMap.Bottom);
             }
-        }        
+        }
         private void window_MouseEnter(object s, MouseEventArgs e)
         {
             this.DropDownMap.Visibility = Visibility.Visible;
             this.close_btn.Visibility = Visibility.Visible;
+            this.cordinate_txt.Visibility = Visibility.Visible;
         }
         private void window_MouseLeave(object s, MouseEventArgs e)
         {
             this.DropDownMap.Visibility = Visibility.Hidden;
             this.close_btn.Visibility = Visibility.Hidden;
-        }        
+            this.cordinate_txt.Visibility= Visibility.Hidden;
+        }
         private void MouseClick_Event(object s, MouseEventArgs e)
         {
             if (isDoubleClick)
             {
                 if (isBig)
                 {
-                    this.MaxHeight = 200;
-                    this.MaxWidth = 200;
-                    this.MinHeight = 200;
-                    this.MinWidth = 200;
-                    
+                    this.MaxHeight = SMWindow;
+                    this.MaxWidth = SMWindow;
+                    this.MinHeight = SMWindow;
+                    this.MinWidth = SMWindow;
                     isBig = false;
                 }
                 else
                 {
-                    this.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight-200;
-                    this.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth-400;
-                    this.MinHeight = System.Windows.SystemParameters.PrimaryScreenHeight-200;
-                    this.MinWidth = System.Windows.SystemParameters.PrimaryScreenWidth-400;
-                    isBig = true;                    
+                    this.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+                    this.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth - 600;
+                    this.MinHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+                    this.MinWidth = System.Windows.SystemParameters.PrimaryScreenWidth - 600;
+                    isBig = true;
                     this.MapImageElement.Margin = new Thickness(0, 0, 0, 0);
                 }
-            } else
+            }
+            else
             {
                 isDoubleClick = true;
                 new Thread(new ThreadStart(ThreadCheckDoubleClick)).Start();
             }
-           
+
         }
         public static void ThreadCheckDoubleClick()
-        {                           
+        {
             Thread.Sleep(250);
-            isDoubleClick = false;            
+            isDoubleClick = false;
         }
-
-        const int PROCESS_WM_READ = 0x0010;
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(int hProcess,
-          IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
 
         private void ReadMemoryOfExanima()
         {
-            Process process = Process.GetProcessesByName("Exanima")[0];
-            if (process == null) return;
+            /*
+             * Get the pointer to the memory of Exanima
+             */
+            Process process;
+            IntPtr baseaddr;
+            try
+            {
+                process = Process.GetProcessesByName("Exanima")[0];
+                baseaddr = process.MainModule.BaseAddress;
 
-            IntPtr baseaddr = process.MainModule.BaseAddress;
-            System.Diagnostics.Debug.WriteLine(process.MainModule.BaseAddress.ToString("X"));
+            }
+            catch
+            {
+                MessageBox.Show("No game named Exanima found. Pls start the game first! And thx for testing :)!", "Big Problem", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Dispatcher.Invoke(new Action(() => { this.Close(); }));
+                return;
+            };
+
+            //System.Diagnostics.Debug.WriteLine(process.MainModule.BaseAddress.ToString("X"));
 
             MemoryHelper32 helper = new MemoryHelper32(process);
 
+            /*
+             * Those are the pointer to X/Y and lvl -> with CheatEngine tryanderror
+             *  
+             */
             uint XPtr = (uint)(0x2A7FF4) + (uint)baseaddr;
-            uint YPtr = (uint)(0x2A7FFC) + (uint)baseaddr; 
+            uint YPtr = (uint)(0x2A7FFC) + (uint)baseaddr;
+            uint LVLPtr = (uint)(0x2137C8) + (uint)baseaddr;
 
-            
+            //offsets: some areas in the game are placed off
+            //TODO: Determine the areas "Position and Size" to get rid of the scaling and the offsets, there has to be an optimal Map.png
+            int offsetX = 0;
+            int offsetY = 0;
+            float scaleF = 20;
 
-            //System.Diagnostics.Debug.WriteLine("DAS IST DER X POINTER -> "+XPtr.ToString("X"));
-            //System.Diagnostics.Debug.WriteLine("DAS IST DER Y POINTER -> " + YPtr.ToString("X"));
-            
-            while (true)
+            while (runThread)
             {
-               
-                //System.Diagnostics.Debug.WriteLine("Y: " + helper.ReadMemory<float>(0x6A7FFC));
-                //System.Diagnostics.Debug.WriteLine("X: " + helper.ReadMemory<float>(0x6A7FF4));
-
+                /**
+                 * READ FROM MEMORY ALL INFORMATION THAT ARE NEEDED
+                 * POSITION, LVL, ITEMS
+                 *
+                 * POSTION
+                 *
+                 * Fetching up X/Y Coordinates [as float]
+                 * The values are in their Engine format. Values between (-50000.0f) to (+50000.0f). 
+                 * For the map(ImageObject) that is scaled 1150/1150 there has to be a mapping/tranformation
+                 *
+                 * Map LVL starts with value:2[as byte] on first floor 
+                 *  
+                 *   2      3       4        5          6         7          8           9            10         11
+                 *  LVL 1 LVL 2   LVL 3   Catacombs   Archive  Crossroads  Golems Crossroads Sewer  Market  Market Sewer
+                 */
 
                 float X = helper.ReadMemory<float>(XPtr);
                 float Y = helper.ReadMemory<float>(YPtr);
-
+                byte mapLvl = helper.ReadMemory<byte>(LVLPtr);
                 //System.Diagnostics.Debug.WriteLine("(X/Y): (" + X+ "/"+ Y +")");
+                //System.Diagnostics.Debug.WriteLine(mapLvl);
+
+
+                /*
+                 * SetOffsets
+                 */
+                if (mapLvl < 5)
+                {
+                    offsetX = 11350;
+                    offsetY = 10900;
+                    scaleF = 20;
+                }
+                else if (mapLvl == 5)
+                {
+                    scaleF = 20;
+                }
+                else if (mapLvl == 6)
+                {
+                    scaleF = 20;
+                }
+                else if (mapLvl == 7)
+                {
+                    offsetX = 20000;
+                    offsetY = 13600;
+                    scaleF = 25;
+                }
+                else if (mapLvl == 8)
+                {
+                    offsetX = 17000;
+                    offsetY = 15700;
+                    scaleF = 27;
+                }
+                else if (mapLvl == 9)
+                {
+
+                }
+                else if (mapLvl == 10)
+                {
+                    offsetX = 16000;
+                    offsetY = 17200;
+                    scaleF = 27.9f;
+
+                }
+                else if (mapLvl == 11)
+                {
+
+                }
+
+
+                //refresh MAP and Location
 
                 this.Dispatcher.Invoke(new Action(() =>
-                {
-                    Thickness thicknessMap = this.MapImageElement.Margin;
-                    Thickness thicknessCPos = this.CPosition.Margin;
-                    //System.Diagnostics.Debug.WriteLine("(X/Y) CIRCLE: (" + thicknessCPos.Left+"/"+thicknessCPos.Top+")");
-                   
-                    this.CPosition.Margin = new Thickness((X+10850) / 20 + thicknessMap.Left, (Y+10750) / 20 + thicknessMap.Top, 0, 0);                    
+                    {
+                        Thickness thicknessMap = this.MapImageElement.Margin;
 
-                }));
-                
+                        posXY[0] = (X + offsetX) / 20;
+                        posXY[1] = (Y + offsetY) / 20;
+
+                        this.DropDownMap.SelectedIndex = mapLvl - 2;
+
+                        if (isBig)
+                        {
+                            this.CPosition.Margin = new Thickness((X + offsetX) / scaleF + thicknessMap.Left, (Y + offsetY) / scaleF + thicknessMap.Top, 0, 0);
+                        }
+                        else
+                        {
+                            this.CPosition.Margin = new Thickness((X + offsetX) / scaleF + thicknessMap.Left, (Y + offsetY) / scaleF + thicknessMap.Top, 0, 0);
+                            Thickness thicknessCPos = this.CPosition.Margin;
+                            this.MapImageElement.Margin = new Thickness(-thicknessCPos.Left + SMWindow / 2 + thicknessMap.Left, -thicknessCPos.Top + SMWindow / 2 + thicknessMap.Top, 0, 0);
+
+                        }
+                        this.cordinate_txt.Text = "(" + posXY[0] + "/" + posXY[1] + ")" + this.DropDownMap.SelectedValue;
+                        //this.cordinate_txt.Visibility = Visibility.Collapsed;
+                    }));
 
                 Thread.Sleep(200);
             }
 
-           
-         
+
+
         }
+
     }
 }
---
+++ ReadMe.txt
@@ -0,0 +1,25 @@
+﻿How-To:
+1. Start the game in window-mode (fullscreen not supported yet)
+2. Begin/Continue ingame
+3. Start the app
+
+
+Modify Maps:
+You can add icons, markers [...] to the [IDX]Map_Y.[png] files found in the folder Maps.
+
+
+Special-Thanks goes to:
+
+-Vismel@Steam for the Catacombs Map
+-Frostdemon727@reddit for the Market Map
+-Tspoon@Steam for the Archive Map and for the impulse to create the whole app
+
+
+
+!Attention!
+
+This App reads the memory of Exanima. 
+In the (near) future it could result in a ban if they release something like a Multiplayer. 
+But at the moment nothing should happen.
+
+!Attention!
